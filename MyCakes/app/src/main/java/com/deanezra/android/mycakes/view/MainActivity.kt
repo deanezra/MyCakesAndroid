e837b53a3220317346ec2da59c00cadfced67e7f
Dean Ezra <141395+deanezra@users.noreply.github.com>
2021-10-27 20:14:43 +0100

    Refresh button added

--- MyCakes/app/src/main/java/com/deanezra/android/mycakes/view/MainActivity.kt
+++ MyCakes/app/src/main/java/com/deanezra/android/mycakes/view/MainActivity.kt
@@ -3,10 +3,15 @@ package com.deanezra.android.mycakes.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.deanezra.android.mycakes.R
 import com.deanezra.android.mycakes.databinding.ActivityMainBinding
+import com.deanezra.android.mycakes.network.NetworkStatus
 import com.deanezra.android.mycakes.reposiitories.CakeRepository
 import com.deanezra.android.mycakes.services.RetrofitService
 import com.deanezra.android.mycakes.viewmodels.MainViewModel
@@ -20,6 +25,24 @@ class MainActivity : AppCompatActivity() {
 
     private val retrofitService = RetrofitService.getInstance()
     val adapter = MainAdapter()
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu to use in the action bar
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle presses on the action bar menu items
+        when (item.itemId) {
+            R.id.action_refresh -> {
+                viewModel.getAllCakes()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,7 +60,28 @@ class MainActivity : AppCompatActivity() {
         })
 
         viewModel.errorMessage.observe(this, Observer {
+            Log.d(TAG, "Error occurred getting cakes from API: $it")
 
+            // TODO: Would be better to use a Material design error popup dialog
+            // With title, icon and error in dialog main body.
+            val text = "Cake list could not be populated. $it"
+            val duration = Toast.LENGTH_LONG
+            val toast = Toast.makeText(applicationContext, text, duration)
+            toast.show()
+        })
+
+        viewModel.networkStatus.observe(this, {
+            when (it) {
+
+                NetworkStatus.ERROR, NetworkStatus.SUCCESS -> {
+                    binding.progressBar.visibility = View.GONE
+                    binding.recyclerview.visibility = View.VISIBLE
+                }
+                NetworkStatus.LOADING -> {
+                    binding.progressBar.visibility = View.GONE
+                    binding.recyclerview.visibility = View.VISIBLE
+                }
+            }
         })
         viewModel.getAllCakes()
     }
